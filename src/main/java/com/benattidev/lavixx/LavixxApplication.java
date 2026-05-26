702a36837b6a729ecafdc13421116737cd94d5fc
Gabriel Wisky Benatti <gabrielwiskybenatti@gmail.com>
2026-05-26 11:51:47 -0300

    feat: iniciar projeto Java 25 + Spring Boot

---
+++ src/main/java/com/benattidev/lavixx/LavixxApplication.java
@@ -0,0 +1,13 @@
+package com.benattidev.lavixx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LavixxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LavixxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/benattidev/lavixx/LavixxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.benattidev.lavixx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LavixxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
